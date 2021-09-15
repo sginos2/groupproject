c35a2a350d4902cd09188ecad8dcb72a0f4d1310
Shanna Ginos <Shanna@Dustins-MacBook-Pro.local>
2021-09-15 10:19:40 -0600

    added router and moved setup from app component to setup component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { SetupComponent } from './components/setup/setup.component';
+import { CardMatchComponent } from './components/card-match/card-match.component';
+import { UserInfoComponent } from './components/user-info/user-info.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent},
+  {path: 'setup', component: SetupComponent},
+  {path: 'match', component: CardMatchComponent},
+  {path: 'user', component: UserInfoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/components/setup/setup.component.ts
+++ src/app/components/setup/setup.component.ts
@@ -12,9 +12,16 @@ interface Set {
 })
 export class SetupComponent implements OnInit {
 
+  sets: Set[] = [
+    {value: 'set-1', viewValue: 'Sword and Shield'},
+    {value: 'set-2', viewValue: 'Sun and Moon'},
+    {value: 'set-3', viewValue: 'XY'},
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
   }
+  
 
 }
