25917d5bc8da1fa3a22c02b6655f7b0f78bb0ef8
Yulissa Gil <yulissagil@Yulissas-MacBook-Pro.local>
2021-09-16 10:16:38 -0600

    starting firebase stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,6 +24,7 @@ import { SetupComponent } from './components/setup/setup.component';
 import {MatSelectModule} from '@angular/material/select';
 import {MatRadioModule} from '@angular/material/radio';
 
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -33,7 +34,7 @@ import {MatRadioModule} from '@angular/material/radio';
     TimerComponent,
     LoginComponent,
     UserInfoComponent,
-    SetupComponent,
+    SetupComponent
  
   ],
   imports: [
@@ -56,4 +57,16 @@ import {MatRadioModule} from '@angular/material/radio';
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCfjJNfvrfoWwQeNn5ZwjvLRSgTJcnhSD0",
+  authDomain: "pokeauth113.firebaseapp.com",
+  projectId: "pokeauth113",
+  storageBucket: "pokeauth113.appspot.com",
+  messagingSenderId: "79644455249",
+  appId: "1:79644455249:web:9819a810fc281e5703a370",
+  measurementId: "G-CV3PQVVLXJ"
+};
+
